56866e61d3fc36cbac6a820c11a9a44f7af3e8cf
mlixytz <mlixytz@gmail.com>
2020-02-24 23:26:21 +0800

    controlProcess“

---
+++ src/chapter1/controlProcess.c
@@ -0,0 +1,29 @@
+#include "apue.h"
+#include <sys/wait.h>
+
+int main(void) {
+    char    buf[MAXLINE];
+    pid_t   pid;
+    int     status;
+
+    printf("%% ");
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if (buf[strlen(buf) - 1] == '\n') {
+            buf[strlen(buf) - 1] = 0; /* execlp 函数要求的参数是以null而不是换行符结束的 */
+        }
+        if ((pid = fork()) < 0) {
+            err_sys("fork error");
+        }
+        if (pid == 0) { /* 进入子进程 */
+            execlp(buf, buf, (char *)0);
+            err_ret("couldn't execute: %s", buf);
+            exit(127);
+        }
+        /* 父进程 */
+        if ((pid = waitpid(pid, &status, 0)) < 0) {
+            err_sys("waitpid error");
+        }
+        printf("%% ");
+    }
+    exit(0);
+}
